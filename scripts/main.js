f6a2ea218365517c386747faf8e8808db67be3bd
etashj <etashjhanji@gmail.com>
2024-09-19 20:50:37 -0400

    Added disks and spinning animation, removed svg files from assets, added resume

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,16 @@ const walkWomanDispText = document.getElementById("walkman-disp");
 const contentDiv = document.querySelector('.content');
 const firstH2 = contentDiv.querySelector('h2');
 let currentHeader = firstH2;
+const diskList = ["cd", "dvd", "blu-ray", "sigma", "cubegame", "umd"];
+let nextDisk = Math.floor(Math.random() * diskList.length);
+let visDisk = 0;
 
+function spinDisk(svg) {
+    const diskGroup = svg.querySelector(".disk-anim-group");
+    // Apply a transition for smooth rotation
+    diskGroup.style.transition = "transform 2.2s ease-out"; // Adjust duration and timing function as needed
+    diskGroup.style.transform = `rotate(${-1*Math.random()*360+3240}deg)`;
+}
 
 window.onload = function() {
     const sections = document.querySelectorAll('.section');
@@ -87,11 +96,13 @@ window.addEventListener('scroll', function() {
 });
 
 document.getElementById("right-skip-svg").onclick = function() {
+    const currentHeaderInd = currentHeader.innerHTML.substring(0, 1)-1;
     document.querySelectorAll('.section-title')[currentHeaderInd+1].scrollIntoView({
         behavior: 'smooth'
     });
 }
 document.getElementById("left-skip-svg").onclick = function() {
+    const currentHeaderInd = currentHeader.innerHTML.substring(0, 1)-1;
     if (currentHeaderInd!=1) {
         document.querySelectorAll('.section-title')[currentHeaderInd-1].scrollIntoView({
             behavior: 'smooth'
@@ -101,5 +112,77 @@ document.getElementById("left-skip-svg").onclick = function() {
             top: 0, 
             behavior: 'smooth'
         });
+    }
+}
+
+const resumeButton = document.getElementById("resume-button");
+const backButton = document.getElementById("back-button");
+
+function switchElements(outgoing1, outgoing2, incoming1, incoming2) {
+    outgoing1.style.transform = "translate(-150%)";
+    outgoing2.style.transform = "translate(150%)";
+    
+    setTimeout(() => {
+        outgoing1.style.display = 'none';
+        outgoing2.style.display = 'none';
+        incoming1.style.display = 'flex';
+        incoming2.style.display = 'flex';
+        incoming1.style.transform = 'translate(-100%)';
+        incoming2.style.transform = 'translate(100%)';
+    }, 500);
+    
+    setTimeout(() => {
+        incoming1.style.transform = 'translate(0%)';
+        incoming2.style.transform = 'translate(0%)';
+    }, 750);
+}
+
+resumeButton.onclick = function() {
+    const tape = document.getElementById("tape");
+    const contentOriginal = document.getElementById("resume-type");
+    const resumeBig = document.getElementById("resume-big"); 
+    const resume = document.getElementById("resume");
+
+    document.getElementById(diskList[nextDisk]).style.display = "block";
+    visDisk = nextDisk; 
+    nextDisk = Math.floor(Math.random() * diskList.length);
+    
+    setTimeout(() => { spinDisk(document.getElementById(diskList[visDisk])); }, 700);
+    
+    switchElements(tape, contentOriginal, resumeBig, resume);
+}
+
+backButton.onclick = function() {
+    const tape = document.getElementById("tape");
+    const contentOriginal = document.getElementById("resume-type");
+    const resumeBig = document.getElementById("resume-big"); 
+    const resume = document.getElementById("resume");
+
+    document.getElementById(diskList[visDisk]).style.display = "none";
+
+    switchElements(resumeBig, resume, tape, contentOriginal);
+}
+
+const diskAnimButton = document.getElementById("change-disc");
+diskAnimButton.onclick = function() {
+    const disk = document.getElementById(diskList[visDisk]);
+    const nDisk = document.getElementById(diskList[nextDisk]);
+    disk.style.transform = 'translate(-350%)';
+    nDisk.style.transform = 'translate(-350%)';
+    setTimeout(() => {
+        disk.style.display = 'none';
+        const diskGroup = disk.querySelector(".disk-anim-group");
+        diskGroup.style.transition = "transform 0s ease-out"; // Adjust duration and timing function as needed
+        diskGroup.style.transform = "rotate(0deg)";
+        nDisk.style.display = 'block';
+        disk.style.transform = 'translate(0%)';
+    }, 500);
+    setTimeout(() => {
+        nDisk.style.transform = 'translate(0%)';
+        spinDisk(nDisk);
+    }, 750);
+    visDisk = nextDisk; 
+    while (nextDisk == visDisk) {
+        nextDisk = Math.floor(Math.random() * diskList.length);
     }
 }
