f60c0a585e716d2abfec94b0386081362559a3f3
Etash Jhanji <etashjhanji@gmail.com>
2025-10-19 07:51:13 -0400

    Update routes

--- src/routes/design/+page.svelte
+++ src/routes/design/+page.svelte
@@ -1,1 +1,4 @@
-Design Coming Soon
+design coming soon
+<a href="/"><div class="my-2 w-fit text-center dark:text-white text-black bg-black/5 dark:bg-white/15 bg-black-5 px-3 py-1 rounded-full transition duration-300 hover:scale-105 active:scale-90">
+    ← back home
+</div></a>
