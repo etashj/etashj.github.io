4d87a734500cb516dddf3884e926d37f9e444d43
Etash Jhanji <etashjhanji@gmail.com>
2025-10-19 07:48:48 -0400

    Update hoempage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,13 @@
         <div class="xs:text-2xl">
             इताश झांजी • cs @ cmu
         </div>
-        <p class="text-sm xs:text-base">+ <a href="mailto:etash@cmu.edu">etash@cmu.edu</a></p>
-        <p class="text-sm xs:text-base">+ <a href="https://linkedin.com/in/etashj">linkedin.com/etashj</a></p>
-        <p class="text-sm xs:text-base">+ <a href="https://github.com/etashj/">github.com/etashj</a></p>
-        <p class="text-sm xs:text-base">+ <a href="https://instagram.com/etashj/">instagram.com/etashj</a></p>
+        <p class="text-sm xs:text-base">+ <a target="_blank" href="mailto:etash@cmu.edu">etash@cmu.edu</a></p>
+        <p class="text-sm xs:text-base">+ <a target="_blank" href="https://linkedin.com/in/etashj">linkedin.com/etashj</a></p>
+        <p class="text-sm xs:text-base">+ <a target="_blank" href="https://github.com/etashj/">github.com/etashj</a></p>
+        <p class="text-sm xs:text-base">+ <a target="_blank" href="https://instagram.com/etashj/">instagram.com/etashj</a></p>
+        <br>
+        <a href="/about"><div class="w-fit text-center dark:text-white text-black bg-black/5 dark:bg-white/15 bg-black-5 px-3 py-1 rounded-full transition duration-300 hover:scale-105 active:scale-90">
+            about →
+        </div></a>
     </div>
     <AsciiModel />
