4758b48f0adf12aa5ec82c6958412aa54cea0c8a
Etash Jhanji <etashjhanji@gmail.com>
2026-01-06 23:06:54 -0500

    Blog copier for static access

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -18,7 +18,7 @@
 				← back to blogs
 			</div></a
 		>
-		<a href="/raw-blog/sample.md" target="_blank" rel="external"
+		<a href="/raw-blog/{data.slug}.md" target="_blank" rel="external"
 			><div
 				class="bg-black-5 my-2 w-fit rounded-lg bg-black/5 px-3 py-1 text-center text-black no-underline transition duration-300 hover:scale-105 active:scale-90 dark:bg-white/15 dark:text-white"
 			>
--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -18,6 +18,7 @@ export const load: PageLoad = async ({ params }) => {
 
   return {
     content: post.default,
-    meta: post.metadata
+    meta: post.metadata,
+    slug: params.slug
   };
 };
