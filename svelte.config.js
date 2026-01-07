d90b7c197b6e727d11504aa98606aefc4eddf1a3
Etash Jhanji <etashjhanji@gmail.com>
2026-01-07 17:20:24 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
       fallback: '404.html'
     }),
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+      base: ''
     },
     alias: {
       '$models': 'src/lib/assets/models', // Alias for the 'src/components' directory
